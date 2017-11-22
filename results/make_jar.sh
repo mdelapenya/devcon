b67eabeaf59cab684142c38c515a82e4c327ec16
Manuel de la Peña <manuel.delapenya@liferay.com>
2017-11-22 12:23:15 +0100

    Always remove jar

--- results/make_jar.sh
+++ results/make_jar.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+rm -f deploy/com.liferay.wedeploy.samples.portlet-1.0.0.jar
 cd src
 zip -r com.liferay.wedeploy.samples.portlet-1.0.0.jar *
 mv com.liferay.wedeploy.samples.portlet-1.0.0.jar ../deploy/
